592084381fee763e1da227d30615b8a878c7acac
acicalo2 <70492912+acicalo2@users.noreply.github.com>
2023-07-28 14:48:13 -0400

    Update 00_run_arc_format.R

--- scripts/00_run_arc_format.R
+++ scripts/00_run_arc_format.R
@@ -29,7 +29,7 @@ if(is.null(args$out_html)) {
 
 rmd_loc <- "00_arc_format.rmd"
 
-file.copy(system.file("rmarkdown/00_arc_format.rmd", package = "scATAC_Seq_Pipeline"),
+file.copy(system.file("rmarkdown/00_arc_format.rmd", package = "scATACSeqPipeline"),
           rmd_loc,
           overwrite = TRUE)
 
